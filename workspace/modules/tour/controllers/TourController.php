44e10892c802443735f91fd2224d2dd9b0352b5e
glambertessa <glambertessa@gmail.com>
2020-09-25 15:29:23 +0300

    view tour

--- workspace/modules/tour/controllers/TourController.php
+++ workspace/modules/tour/controllers/TourController.php
@@ -3,15 +3,12 @@ namespace workspace\modules\tour\controllers;
 
 use core\App;
 use core\Controller;
-use core\Debug;
 use workspace\modules\additional_price\models\AdditionalPrice;
 use workspace\modules\date\models\Date;
 use workspace\modules\feature\models\Feature;
 use workspace\modules\image\models\Image;
 use workspace\modules\included\models\Included;
-use workspace\modules\plan\controllers\PlanController;
 use workspace\modules\plan\models\Plan;
-use workspace\modules\plan\requests\PlanSearchRequest;
 use workspace\modules\section\models\Section;
 use workspace\modules\tour\models\Tour;
 use workspace\modules\tour\requests\TourSearchRequest;
@@ -40,7 +37,90 @@ class TourController extends Controller
     {
         $model = Tour::where('id', $id)->first();
 
-        return $this->render('tour/view.tpl', ['model' => $model, 'options' => $this->getOptions()]);
+        $common_options = [
+            'serial' => '#',
+            'thead_class' => '.thead-light',
+            'tr_class' => 'fixed-height',
+            'td_class' => 'fixed-height',
+            'filters' => false,
+        ];
+
+        $plans = Plan::where('tour_id', $model->id)->get();
+        $plans_opt = [
+            'fields' => [
+                'day' => 'День',
+                'info' => 'Информация',
+                'date' => 'Даты',
+                'description' => 'Описание',
+            ],
+            'baseUri' => '/admin/plan',
+        ];
+        $plans_options = array_merge($common_options, $plans_opt);
+
+        $features = Feature::where('tour_id', $model->id)->get();
+        $features_opt = [
+            'fields' => [
+                'feature' => 'Особенность',
+                'type' => 'Тип',
+            ],
+            'baseUri' => '/admin/feature',
+        ];
+        $features_options = array_merge($common_options, $features_opt);
+
+        $included = Included::where('tour_id', $model->id)->get();
+        $included_opt = [
+            'fields' => [
+                '_column_side' => [
+                    'label' => 'Сторона',
+                    'value' => function($model) {
+                        if ($model->column_side === 0) {
+                            return 'Левая сторона';
+                        } else {
+                            return 'Правая сторона';
+                        }
+                    }
+                ],
+                'text' => 'Текст',
+            ],
+            'baseUri' => '/admin/included',
+        ];
+        $included_options = array_merge($common_options, $included_opt);
+
+        $dates = Date::where('tour_id', $model->id)->get();
+        $dates_opt = [
+            'fields' => [
+                'tour_dates' => 'Даты',
+                'remaining_places' => 'Оставшиеся места',
+            ],
+            'baseUri' => '/admin/date',
+        ];
+        $dates_options = array_merge($common_options, $dates_opt);
+
+        $additional_prices = AdditionalPrice::where('tour_id', $model->id)->get();
+        $additional_prices_opt = [
+            'fields' => [
+                'text' => 'Доп. оплачивается',
+            ],
+            'baseUri' => '/admin/additional_price',
+        ];
+        $additional_prices_options = array_merge($common_options, $additional_prices_opt);
+
+        $sections = Section::where('tour_id', $model->id)->get();
+        $sections_opt = [
+            'fields' => [
+                'name' => 'Название',
+                'text' => 'Текст',
+                'priority' => 'Приоритет',
+            ],
+            'baseUri' => '/admin/section',
+        ];
+        $sections_options = array_merge($common_options, $sections_opt);
+
+
+        return $this->render('tour/view.tpl', ['model' => $model, 'options' => $this->getOptions(),  'plans' => $plans, 'plans_options' => $plans_options, 'features' => $features, 'features_options' => $features_options,
+            'included' => $included, 'included_options' => $included_options, 'dates' => $dates, 'dates_options' => $dates_options,
+            'additional_prices' => $additional_prices, 'additional_prices_options' => $additional_prices_options,
+            'sections' => $sections, 'sections_options' => $sections_options]);
     }
 
     public function actionStore()
--- workspace/modules/tour/views/tour/view.tpl
+++ workspace/modules/tour/views/tour/view.tpl
@@ -3,3 +3,21 @@
 <div class="h1">{$model->name}</div>
 
 {core\DetailView::widget()->setParams($model, $options)->run()}
+
+<div class="h3">Планы на день <a href="/admin/plan/create" class="btn btn-outline-dark">Создать</a></div>
+{core\GridView::widget()->setParams($plans, $plans_options)->run()}
+
+<div class="h3">Особенности тура <a href="/admin/feature/create" class="btn btn-outline-dark">Создать</a></div>
+{core\GridView::widget()->setParams($features, $features_options)->run()}
+
+<div class="h3">Включено в тур <a href="/admin/included/create" class="btn btn-outline-dark">Создать</a></div>
+{core\GridView::widget()->setParams($included, $included_options)->run()}
+
+<div class="h3">Даты <a href="/admin/date/create" class="btn btn-outline-dark">Создать</a></div>
+{core\GridView::widget()->setParams($dates, $dates_options)->run()}
+
+<div class="h3">Доп. оплата <a href="/admin/additional_price/create" class="btn btn-outline-dark">Создать</a></div>
+{core\GridView::widget()->setParams($additional_prices, $additional_prices_options)->run()}
+
+<div class="h2">Разделы <a href="/admin/section/create" class="btn btn-outline-dark">Создать</a></div>
+{core\GridView::widget()->setParams($sections, $sections_options)->run()}
